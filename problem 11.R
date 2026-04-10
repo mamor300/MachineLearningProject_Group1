b346ab7b93a16ef4ec72f7ecb0ea271fc330b16d
whaler223 <dannytemprano@gmail.com>
2026-04-09 20:30:24 -0400

    Update problem 11.R

--- problem 11.R
+++ problem 11.R
@@ -1,20 +1,26 @@
-#11
+#11 (my code now)
 # 5 clusters
 library(clustMixType)
 #creating a matrix for mixed type cluster analysis
 #using share of people of color, average household income, whether older or younger, Principal Component 1, is_older_county, and whether a legislature is republican controlled or not
 # Create a named vector of Republican-controlled legislatures in 2024
-republican_states <- c("AL", "AZ", "AR", "FL", "GA", "ID", "IN", "IA", 
-                       "KS", "KY", "LA", "MS", "MO", "MT", "NH", "ND", 
-                       "OH", "OK", "SC", "SD", "TN", "TX", "UT", "WV", "WY",
-                       "WI", "NC")
+#republican_states <- c("AL", "AZ", "AR", "FL", "GA", "ID", "IN", "IA", 
+                       #"KS", "KY", "LA", "MS", "MO", "MT", "NH", "ND", 
+                       #"OH", "OK", "SC", "SD", "TN", "TX", "UT", "WV", "WY",
+                       #"WI", "NC")
 
-CFPB$rep_legislature <- as.factor(ifelse(CFPB$State %in% republican_states, 1, 0))
-matrix1 <- bind_cols(c(CFPB[,c("PC1", "Year", "rep_legislature")], CFPB.debt[,c("Share of people of color", "Average household income, All")], CFPB_Census[,c("prop_young", "prop_65plus", "is_older_county")]))
+#CFPB$rep_legislature <- as.factor(ifelse(CFPB$State %in% republican_states, 1, 0))
+matrix1 <- bind_cols(c(CFPB[,c("PC1", "Year", "Issue")], CFPB.debt[,c("Share of people of color")], CFPB_Census[,c("prop_young", "prop_65plus")]))
 CFPB_clust<- matrix1
+#I tried adding the variables I got from sahie but it made the lambda so large I don't think it's worth it (498314632027)
 #CFPB_clust$is_servicemember <- as.factor(CFPB_clust$is_servicemember)
-CFPB_clust$is_older_county <- as.factor(CFPB_clust$is_older_county)
+#CFPB_clust$is_older_county <- as.factor(CFPB_clust$is_older_county)
+#removing the older county thing here actually seems to improve it
+#for whatever reason (idk why) but adding issue and sub.issue as categorical variables is lowering the lambda here
+#my republic legislature thing also seemed to make it worse
+#the household income variable was shooting up the lambda to several million, deleting this greatly improved it
 kpres <- kproto(x = CFPB_clust, k = 5)
+#managed to get it down to 2.4
 kpres
 summary(kpres)
 library(wesanderson)
