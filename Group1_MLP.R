afd60f44eef7321432c92b612a948983f16e913c
mamor300 <m.amor@gwu.edu>
2026-04-12 15:40:24 -0400

    Update Group1_MLP.R

--- Group1_MLP.R
+++ Group1_MLP.R
@@ -13,7 +13,8 @@ pacman::p_load(
   missMDA,
   tidycensus,
   randomForest,
-  clustMixType)
+  clustMixType,
+  censusapi)
 
 #1. 
 {
@@ -464,6 +465,27 @@ CFPB8 <- CFPB.FMR %>%
 #Verify the results 
 # This should show the counts for each year 
 table(CFPB8$Year)
+Sys.setenv(CENSUS_KEY = "ba79958600ff02f01da8a857d6a3243c191cfc8a")
+sahie_vars <- listCensusMetadata(
+  name = "timeseries/healthins/sahie",
+  type = "variables"
+)
+#this will show you what all the variables chosen below are
+View(sahie_vars)
+library(purrr)
+#no 2024 or 2025 data
+sahie <- map_dfr(2022:2023, ~getCensus(
+  name   = "timeseries/healthins/sahie",
+  vars   = c("NAME", "PCTUI_PT", "NUI_PT", "NIC_PT","NIPR_PT", "YEAR"),
+  region = "county:*",
+  time   = .x
+))|>
+  mutate(fips = paste0(state, county)) |>
+  select(-time) |>
+  rename(Year = YEAR, FIPS = fips)
+sahie$Year <- as.numeric(sahie$Year)
+CFPB.sahie <- CFPB8 |>
+  left_join(sahie,by = c('FIPS',"Year"))
 }
 #9.
 {
