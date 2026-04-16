08fa102eb89dc3ec65ff4e5a84c128cc2f47fa19
whaler223 <dannytemprano@gmail.com>
2026-04-15 20:51:38 -0400

    Update XGBoost.R

--- XGBoost.R
+++ XGBoost.R
@@ -1,4 +1,3 @@
-#13
 
 #XGBoost model
 library(dplyr)
@@ -7,8 +6,10 @@ library(xgboost)
 library(caret)
 library(Matrix)
 library(data.table)
-CFPB <- read_excel("C:/Users/wh00ler/Desktop/Machine Learning/project/CFPB.xlsx")
+library(readxl)
+CFPB <- read_excel("C:/Users/whaler/Downloads/CFPB.xlsx")
 y <- as.numeric(CFPB$Relief)
+y_factor <- as.factor(CFPB$Relief)
 #this is removing tags from the xgboost model parameters
 #I had to do this colnames thing since sparse.model.matrix was giving a name mismatch error
 colnames(CFPB) <- make.names(colnames(CFPB))
@@ -17,7 +18,7 @@ train_control <- trainControl(method = "cv", number = 5)
 XGBoostdata <- xgb.DMatrix(data = x, label = y)
 XGBparams <- list(
   booster = "gbtree",
-  objective = "reg:squarederror",
+  objective = "binary:logistic",
   eta = 0.05,
   max_depth = 6,
   min_child_weight = 10,
@@ -27,6 +28,7 @@ XGBparams <- list(
   subsample = 0.8,
   verbosity = 1
 )
+set.seed(123)
 #find optimal number of rounds
 system.time ({
   XGBoostCV <- xgb.cv(
@@ -34,10 +36,10 @@ system.time ({
     params = XGBparams,
     nrounds = 200,
     nfold = 5,
-    metrics = "rmse"
+    metrics = "error"
   )
 })
-XGBnrounds <- which.min(XGBoostCV$evaluation_log$test_rmse_mean)
+XGBnrounds <- which.min(XGBoostCV$evaluation_log$test_error_mean)
 XGBnrounds
 system.time({
   XGBmodel <- xgb.train(
@@ -66,10 +68,30 @@ tune_control <- caret::trainControl(
   allowParallel = TRUE #FALSE for reproducible results
 )
 system.time({xgb_tune <- caret::train(
-  x = x, y = y,
+  x = x, y = y_factor,
   trControl = tune_control,
   tuneGrid = tune_grid,
   method = "xgbTree",
   verbose = TRUE
 )
 })
+xgb_tune$bestTune
+max(xgb_tune$results$Accuracy)
+qqnorm(XGBresiduals)
+library(DiagrammeR)
+xgb.plot.tree(model = xgb_tune$finalModel, trees = 1)
+### plot
+#get the first three trees
+xgb.plot.tree(model = xgb_tune$finalModel, trees = 0:2)
+xgb.plot.multi.trees(xgb_tune$finalModel)
+importance_matrix <- xgb.importance(model = xgb_tune$finalModel)
+xgb.plot.importance(importance_matrix, xlab = "Feature Importance")
+#accuracy
+XGBpredictions_class <- ifelse(XGBpredictions > 0.5, 1, 0)
+accuracy <- mean(XGBpredictions_class == y)
+accuracy
+#accuracy <- 1 - min(XGBoostCV2$evaluation_log$test_error_mean)
+#accuracy
+#confusion matrix
+table(XGBpredictions_class, y)
+caret::confusionMatrix(as.factor(XGBpredictions_class), as.factor(y))
